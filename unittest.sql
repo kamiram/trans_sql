fdd89c92127c8756072f373ec329258feebacb4c
Mikhail Kalinin <kalinin.mr@gmail.com>
2022-10-07 15:47:12 +0300

    add test for override

--- unittest.sql
+++ unittest.sql
@@ -6,6 +6,7 @@ select * from trans.trans_id_bk ;
 insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (1, 21,'2021-06-28 07:09:19','2021-06-28 07:09:19', 4);
 insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (2, 22,'2021-06-28 07:09:19','2021-06-28 07:09:19', 5);
 insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (3, 23,'2021-06-28 07:09:19','2021-06-28 07:09:19', 4);
+insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (5, 23,'2021-06-28 07:09:19','2021-06-28 07:09:19', 4);
 
 
 select * from trans.trans_id_bk ;

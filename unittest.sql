79bfa1dd6b5f04bd9c495332310004bf1fa03986
Mikhail Kalinin <kalinin.mr@gmail.com>
2022-10-07 15:32:22 +0300

    unittest

---
+++ unittest.sql
@@ -0,0 +1,6 @@
+\i triggers.sql
+select * from trans.trans_id_bk ;
+
+insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (1, 21,'2021-06-28 07:09:19','2021-06-28 07:09:19', 4);
+insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (2, 22,'2021-06-28 07:09:19','2021-06-28 07:09:19', 5);
+insert into trans.trans_coord (id,equip_id,gnss_time,equip_time, cycle_id) values (3, 23,'2021-06-28 07:09:19','2021-06-28 07:09:19', 4);
